3ba62ccaef1d0b02d3ba035075002c044632c8c6
Abdul Wahab <abdulwahabdev0@gmail.com>
2024-05-15 10:38:36 +0500

    model , static file  , templates etc

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course
+@admin.register(Course)
+class Admincourse(admin.ModelAdmin):
+    list_display = ('id' , 'name')
 
-# Register your models here.
+# admin.site.register(Course , Admincourse)
+
--- course/views.py
+++ course/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from course.models import Course
 # Create your views here.
 
 
 
 def index(request):
+    data = Course.objects.all()
+    
     learning = {"name":"django"}
-    return render(request , "course1.html",context=learning)
+    
+    return render(request , "course1.html",context={"data":data})
 
 def test(request):
     return HttpResponse("OK")
